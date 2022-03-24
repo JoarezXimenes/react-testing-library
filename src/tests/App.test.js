f74a50ca835f589cafb47340003f2003567fe71b
JoarezXimenes <joarezximenesleitejunior@gmail.com>
2022-03-24 11:26:37 -0300

    requisito 1 completo

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,16 +1,51 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
 describe('Testa o componente App', () => {
-  it('Testa o texto de cada link', () => {
+  it('testa se o link home foi renderizado', () => {
     renderWithRouter(<App />);
     const homeLink = screen.getByRole('link', { name: /home/i });
-    const aboutLink = screen.getByRole('link', { name: /about/i });
-    const favLink = screen.getByRole('link', { name: /Favorite Pokémons/i });
     expect(homeLink).toBeInTheDocument();
+  });
+
+  it('testa se o link about foi renderizado', () => {
+    renderWithRouter(<App />);
+    const aboutLink = screen.getByRole('link', { name: /about/i });
     expect(aboutLink).toBeInTheDocument();
+  });
+
+  it('testa se o link Favorite Pokémons foi renderizado', () => {
+    renderWithRouter(<App />);
+    const favLink = screen.getByRole('link', { name: /Favorite Pokémons/i });
     expect(favLink).toBeInTheDocument();
+  });
+});
+
+describe('Testa o funcionamento dos links', () => {
+  it('testa se o link Home leva para url /', () => {
+    const { history } = renderWithRouter(<App />);
+    const homeLink = screen.getByRole('link', { name: /home/i });
+    userEvent.click(homeLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('testa se o link about leva para url /about', () => {
+    const { history } = renderWithRouter(<App />);
+    const aboutLink = screen.getByRole('link', { name: /about/i });
+    userEvent.click(aboutLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('testa se o link Favorite Pokémons leva para url /favorites', () => {
+    const { history } = renderWithRouter(<App />);
+    const favLink = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    userEvent.click(favLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
   });
 });
