b7a8b8f2e5cd5c065a27cf542c4402000528a88e
JoarezXimenes <joarezximenesleitejunior@gmail.com>
2022-03-24 10:51:48 -0300

    metade do requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente App', () => {
+  it('Testa o texto de cada link', () => {
+    renderWithRouter(<App />);
+    const homeLink = screen.getByRole('link', { name: /home/i });
+    const aboutLink = screen.getByRole('link', { name: /about/i });
+    const favLink = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    expect(homeLink).toBeInTheDocument();
+    expect(aboutLink).toBeInTheDocument();
+    expect(favLink).toBeInTheDocument();
+  });
+});
