e1399e5a5176e3b91cf60f31881403891871a72f
JoarezXimenes <joarezximenesleitejunior@gmail.com>
2022-03-26 05:38:13 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Testa a página NotFound', () => {
+  it('testa de o texto Page requested not found é exibido', () => {
+    renderWithRouter(<NotFound />);
+    const notFound = screen.getByRole('heading', { name: /Page requested not found/i });
+    expect(notFound).toBeInTheDocument();
+  });
+  it('testa se a imagem correta é renderizada', () => {
+    renderWithRouter(<NotFound />);
+    const imgAlt = 'Pikachu crying because the page requested was not found';
+    const pikachuImg = screen.getByRole('img', { name: imgAlt });
+    expect(pikachuImg).toBeInTheDocument();
+    expect(pikachuImg.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
